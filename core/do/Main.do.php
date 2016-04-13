45e110dee17db5f3ab54011fd5b08a5a47727c30
Ltre Oreki <Ltre@users.noreply.github.com>
2016-04-13 02:35:40 -0500

    设置CMS的demo

--- core/do/Main.do.php
+++ core/do/Main.do.php
@@ -6,4 +6,25 @@ class MainDo extends DIDo {
         die('<div style="width:100%; text-align:center; position:fixed; bottom:0; margin-bottom:10px; font-size: 19px; font-family:微软雅黑;"><a href="http://larele.com/">Ltre Inc.</a> &copy; <a href="http://acggeek.com/">ACG极客</a> · <a href="http://www.miitbeian.gov.cn/">粤ICP备15066774号-3</a></div>');
     }
     
+    function mirror(){
+        $h = '<!DOCTYPE html><html><body>';
+        $h .= '<form id="mirrorform" action="/" method="post" target="tmp">';
+        $h .= '<input id="path" name="setmirror">';
+        $h .= '<textarea name="data"></textarea>';
+        $h .= '<input type="submit">';
+        $h .= '</form>';
+        $h .= '<iframe name="tmp" style="display:none;"></iframe>';
+        $h .= '<script>
+            function hehe(){
+                var domPath = document.getElementById("path");
+                var path = domPath.value;
+                domPath.name = "setmirror/" + path;
+                domPath.value = "";
+            }
+            document.getElementsByTagName("form")[0].setAttribute("onsubmit", "return hehe()");
+        </script>';
+        $h .= '</body></html>';
+        echo $h;
+    }
+    
 }
