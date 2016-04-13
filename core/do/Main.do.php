d9af5ca2bb20009d6a6f1d189230f6746cbd98bd
Ltre Oreki <Ltre@users.noreply.github.com>
2016-04-13 05:50:22 -0500

    main/mirror可根据path增加或编辑内容

--- core/do/Main.do.php
+++ core/do/Main.do.php
@@ -6,22 +6,22 @@ class MainDo extends DIDo {
         die('<div style="width:100%; text-align:center; position:fixed; bottom:0; margin-bottom:10px; font-size: 19px; font-family:微软雅黑;"><a href="http://larele.com/">Ltre Inc.</a> &copy; <a href="http://acggeek.com/">ACG极客</a> · <a href="http://www.miitbeian.gov.cn/">粤ICP备15066774号-3</a></div>');
     }
     
+    //可根据path增加或编辑内容
     function mirror(){
+        $path = arg('path') ?: '';
         $h = '<!DOCTYPE html><html><body>';
-        $h .= '<form id="mirrorform" action="/" method="post" target="tmp">';
-        $h .= '<input id="path" name="setmirror"><br>';
+        $h .= '<form action="/" method="post" target="tmp">';
+        $h .= '<input id="path" name="setmirror/'.$path.'" type="hidden"><br>';
+        $h .= '<input id="on-writing-path" value="'.$path.'"><br>';
         $h .= '<textarea name="data"></textarea><br>';
         $h .= '<input type="submit">';
         $h .= '</form>';
         $h .= '<iframe name="tmp" style="display:none;"></iframe>';
         $h .= '<script>
-            function hehe(){
-                var domPath = document.getElementById("path");
-                var path = domPath.value;
-                domPath.name = "setmirror/" + path;
-                domPath.value = "";
-            }
-            document.getElementsByTagName("form")[0].setAttribute("onsubmit", "return hehe()");
+            document.getElementById("on-writing-path").onkeyup = function(){
+                document.getElementById("path").name = "setmirror/" + this.value;
+                console.log(document.getElementById("path").name);
+            };
         </script>';
         $h .= '</body></html>';
         echo $h;
