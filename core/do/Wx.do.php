e8f1872d23bc073f1dcd112a0dcc7df5c332bee8
Ltre Oreki <Ltre@users.noreply.github.com>
2016-06-23 22:43:23 +0800

    wx validate

--- core/do/Wx.do.php
+++ core/do/Wx.do.php
@@ -4,8 +4,54 @@
  */
 class WxDo extends DIDo {
     
+    function start(){
+        $signature = arg('signature');
+        $timestamp = arg('timestamp');
+        $nonce = arg('nonce');
+        $echoStr = arg('echostr');
+        if($this->checkSignature()){
+            echo $echoStr;
+            exit;
+        }
+    }
+    
+    
     function setAppSecret($secret){
         file_put_contents(DI_DATA_PATH.'wechat.appsecret', $secret);
+    }
+    
+    
+    function setToken($token){
+        file_put_contents(DI_DATA_PATH.'wechat.token', $token);
+    }
+    
+    
+    private function _getAppSecret(){
+        return @file_get_contents(DI_DATA_PATH.'wechat.appsecret') ?: '';
+    }
+    
+    
+    private function _getToken(){
+        return @file_get_contents(DI_DATA_PATH.'wechat.token') ?: '';
+    }
+    
+    
+    private function checkSignature($signature, $timestamp, $nonce){
+        $token = $this->_getToken();
+        if (empty($token)) {
+            throw new Exception('TOKEN is not defined!');
+        }
+    
+        $tmpArr = array($token, $timestamp, $nonce);
+        sort($tmpArr, SORT_STRING);
+        $tmpStr = implode( $tmpArr );
+        $tmpStr = sha1( $tmpStr );
+    
+        if ($tmpStr == $signature) {
+            return true;
+        } else {
+            return false;
+        }
     }
     
 }
