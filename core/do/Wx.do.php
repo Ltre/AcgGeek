51867a2b85e717b78d923cbffa986d43a9b7b692
Ltre <ltrele@yeah.net>
2016-07-05 23:08:21 +0800

    1

--- core/do/Wx.do.php
+++ core/do/Wx.do.php
@@ -12,10 +12,8 @@ class WxDo extends DIDo {
         $nonce = arg('nonce');
         $echoStr = arg('echostr');
         if (isset($_GET['echostr'])) { //验证公众号绑定
-            if (WxValidate::checkBind($signature, $timestamp, $nonce)) {
-                echo $echoStr;
-                exit;
-            }
+            if (WxValidate::checkBind($signature, $timestamp, $nonce)) echo $echoStr;
+            exit;
         } else { //其它类型请求
             $postStr = @$GLOBALS["HTTP_RAW_POST_DATA"] ?: file_get_contents("php://input");
             echo WxMsg::getResponse($postStr);
